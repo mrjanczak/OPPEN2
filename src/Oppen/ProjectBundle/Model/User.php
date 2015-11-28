520dfda4b874e26865ec9658b756345657a1eefe
mrjanczak <mrjanczak@gmail.com>
2015-11-28 01:15:41 +0100

    migration to heroku

---
+++ src/Oppen/ProjectBundle/Model/User.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Oppen\ProjectBundle\Model;
+
+use Oppen\ProjectBundle\Model\om\BaseUser;
+ // FOS\UserBundle\Propel\om\BaseUser;
+
+class User extends BaseUser
+{
+}
---
+++ src/Oppen/ProjectBundle/Model/UserQuery.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Oppen\ProjectBundle\Model;
+
+use Oppen\ProjectBundle\Model\om\BaseUserQuery;
+ // FOS\UserBundle\Propel\om\BaseUserQuery
+    
+class UserQuery extends BaseUserQuery
+{
+}
