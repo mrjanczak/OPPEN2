e3c360b46a16229d251dd4e0180d7a9bb5a66f61
mrjanczak <mrjanczak@gmail.com>
2015-12-07 23:40:24 +0100

    port

--- app/config/parameters_prod.php
+++ app/config/parameters_prod.php
@@ -1,15 +1,22 @@
 <?php
-    $db = parse_url(getenv('CLEARDB_DATABASE_URL'));
 
-    $container->setParameter('database_driver', 'pdo_mysql');
-    $container->setParameter('database_host', $db['host']);
-    $container->setParameter('database_port', $db['port']);
-    $container->setParameter('database_name', substr($db["path"], 1));
-    $container->setParameter('database_user', $db['user']);
-    $container->setParameter('database_password', $db['pass']);
-    $container->setParameter('secret', getenv('SECRET'));
-    $container->setParameter('locale', 'pl');
-    $container->setParameter('mailer_transport', null);
-    $container->setParameter('mailer_host', null);
-    $container->setParameter('mailer_user', null);
-    $container->setParameter('mailer_password', null);
+// Using ClearDB for mysql database
+$db = parse_url($_ENV['CLEARDB_DATABASE_URL']); 
+$container->setParameter('database_driver', 'pdo_mysql'); 
+$container->setParameter('database_host', $db['host']); 
+$container->setParameter('database_port', '~');
+$container->setParameter('database_name', trim($db['path'], '/')); 
+$container->setParameter('database_user', $db['user']); 
+$container->setParameter('database_password', $db['pass']);
+
+// Using Mandrill to send email
+$container->setParameter('mailer_transport', 'smtp');
+$container->setParameter('mailer_host', 'smtp.mandrillapp.com');
+$container->setParameter('mailer_user', $_ENV['MANDRILL_USERNAME']);
+$container->setParameter('mailer_password', $_ENV['MANDRILL_APIKEY']);
+
+// Other settings
+$container->setParameter('locale', 'pl');
+$container->setParameter('secret', 'c5bd9c14be810cd74439e07bdbc933049e092c5c');
+
+?>
