6bbda155194f5726f1557d53d4a4aa57d0aa41f9
mrjanczak <mrjanczak@gmail.com>
2015-12-07 23:48:07 +0100

    parameters.php

--- app/config/parameters.php
+++ app/config/parameters.php
@@ -10,10 +10,12 @@ $container->setParameter('database_user', $db['user']);
 $container->setParameter('database_password', $db['pass']);
 
 // Using Mandrill to send email
+/*
 $container->setParameter('mailer_transport', 'smtp');
 $container->setParameter('mailer_host', 'smtp.mandrillapp.com');
 $container->setParameter('mailer_user', $_ENV['MANDRILL_USERNAME']);
 $container->setParameter('mailer_password', $_ENV['MANDRILL_APIKEY']);
+*/
 
 // Other settings
 $container->setParameter('locale', 'pl');
