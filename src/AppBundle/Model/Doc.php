8b6e2dcbd1628487f57cc1fe69ae1183b1256c5b
mrjanczak <mrjanczak@gmail.com>
2019-07-12 08:57:55 +0200

    Update Doc.php

--- src/AppBundle/Model/Doc.php
+++ src/AppBundle/Model/Doc.php
@@ -79,7 +79,7 @@ class Doc extends BaseDoc
 		$tmp = str_replace('#M',   $M,     $tmp);
 		$tmp = str_replace('#Y',   $Y,     $tmp);
 		$tmp = str_replace('#PROJ',$PROJ,  $tmp);
-		$tmp = str_replace('#C',   '0'.$COST,  $tmp);
+		$tmp = str_replace('#COST',$COST,  $tmp);
 		$tmp = str_replace('#NAME',$NAME,  $tmp);
 		
 		return $this->setDocNo($tmp);
