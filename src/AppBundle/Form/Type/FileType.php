48cd78e0275968281d4a86ac372724598cebb06b
mrjanczak <mrjanczak@gmail.com>
2020-09-07 23:50:07 +0200

    add Mikrorachunek podatkowy

--- src/AppBundle/Form/Type/FileType.php
+++ src/AppBundle/Form/Type/FileType.php
@@ -88,11 +88,11 @@ class FileType extends AbstractType
 			->add('country', 'country', array('label' => 'Kraj', 'required' => false, )) 
 
             ->add('postOffice', 'text', array('label' => 'Poczta','required' => false))
-            
-			->add('bankAccount', 'text', array('label' => 'Krajowe Konto bankowe','required' => false, 'attr' => array('size' => 30)) )
-			
-			->add('bankIBAN', 'text', array('label' => 'IBAN','required' => false, 'attr' => array('size' => 30)) )
-			->add('bankSWIFT', 'text', array('label' => 'SWIFT','required' => false, 'attr' => array('size' => 30)) )
+		->add('bankTaxAccount', 'text', array('label' => 'Mikrorachunek podatkowy','required' => false, 'attr' => array('size' => 30)) )
+		->add('bankAccount', 'text', array('label' => 'Krajowe Konto bankowe','required' => false, 'attr' => array('size' => 30)) )
+
+		->add('bankIBAN', 'text', array('label' => 'IBAN','required' => false, 'attr' => array('size' => 30)) )
+		->add('bankSWIFT', 'text', array('label' => 'SWIFT','required' => false, 'attr' => array('size' => 30)) )
             ->add('bankName', 'text', array('label' => 'Nazwa banku','required' => false))
             
             ->add('phone', 'text', array('label' => 'telefon','required' => false))
