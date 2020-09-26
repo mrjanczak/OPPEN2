cfb19599b5590e326d5a0bb8170d7bcda0258384
mrjanczak <mrjanczak@gmail.com>
2020-09-27 01:01:33 +0200

    Update AppExtension.php

--- src/AppBundle/Twig/AppExtension.php
+++ src/AppBundle/Twig/AppExtension.php
@@ -16,31 +16,32 @@ class AppExtension extends \Twig_Extension
     public function getFilters()
     {
         return array(
-            new \Twig_SimpleFilter(
-                'md2html',
-                array($this, 'markdownToHtml'),
-                array('is_safe' => array('html'))
-            ),
-             new \Twig_SimpleFilter(
-                'pl2ascii',
-                array($this, 'pl2asciiFilter'),
-            ),           
-            
+            new \Twig_SimpleFilter('md2html', array($this, 'markdownToHtml') ),
+            new \Twig_SimpleFilter('pl2ascii',array($this, 'pl2asciiFilter') ),                       
         );
     }
 
+    /**
+     * markdownToHtml string
+     */    
     public function markdownToHtml($content)
     {
         return $this->parser->toHtml($content);
     }
     
+    /**
+     * pl2asciiFilter string
+     */    
     public function pl2asciiFilter($content)
     {
         $search  = array('ą','ć','ę','ł','ń','ó','ś','ż','ź','Ą','Ć','Ę','Ł','Ń','Ó','Ś','Ż','Ź',);
         $replace = array('a','c','e','l','n','o','s','z','z','A','C','E','L','N','O','S','Z','Z',);
         return str_replace($search, $replace, $content);        
     }    
-
+    
+    /**
+     * {@inheritdoc}
+     */
     public function getName()
     {
         return 'twig_extension';
