6c0806469ed0f5125d05a5eaa54cd7a213cf8a47
mrjanczak <mrjanczak@gmail.com>
2020-09-27 01:09:38 +0200

    Update AppExtension.php

--- src/AppBundle/Twig/AppExtension.php
+++ src/AppBundle/Twig/AppExtension.php
@@ -2,31 +2,15 @@
 
 namespace AppBundle\Twig;
 
-use AppBundle\Utils\Markdown;
-
 class AppExtension extends \Twig_Extension
 {
-    private $parser;
 
-    public function __construct(Markdown $parser)
-    {
-        $this->parser = $parser;
-    }
 
     public function getFilters()
     {
         return array(
-            new \Twig_SimpleFilter('md2html', array($this, 'markdownToHtml') ),
             new \Twig_SimpleFilter('pl2ascii',array($this, 'pl2asciiFilter') ),                       
         );
-    }
-
-    /**
-     * markdownToHtml string
-     */    
-    public function markdownToHtml($content)
-    {
-        return $this->parser->toHtml($content);
     }
     
     /**
